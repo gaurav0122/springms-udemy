6963f372d31b643ec9dd1957d224911cfd9e9234
Gaurav <gauravlavand0102@gmail.com>
2023-11-23 20:50:04 +0530

    hands-on till section 5

--- crud-rest-api/src/main/java/com/net/CrudRestApiApplication.java
+++ crud-rest-api/src/main/java/com/net/CrudRestApiApplication.java
@@ -1,7 +1,9 @@
 package com.net;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudRestApiApplication {
@@ -10,4 +12,8 @@ public class CrudRestApiApplication {
 		SpringApplication.run(CrudRestApiApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper getModelMapper() {
+		return new ModelMapper();
+	}
 }
--- crud-rest-api/src/main/java/com/net/service/UserServiceImpl.java
+++ crud-rest-api/src/main/java/com/net/service/UserServiceImpl.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import com.net.dto.UserDto;
 import com.net.entity.User;
+import com.net.mapper.MapStructMapper;
 import com.net.mapper.UserMapper;
 import com.net.repository.UserRepository;
 
@@ -23,28 +25,56 @@ public class UserServiceImpl implements UserService{
 	
 	private UserRepository userRepository;
 	
+	private ModelMapper modelMapper;
+	
 	@Override
 	public UserDto createuser(UserDto userdto) {
-		User user = UserMapper.userDtotoUser(userdto);
+		//using my custom mapper
+//		User user = UserMapper.userDtotoUser(userdto);
+		
+		//using modelmapper
+//		User user = modelMapper.map(userdto, User.class);
+		
+		//using mapstruct
+		User user = MapStructMapper.Mapper.mapToUser(userdto);
 
 		User savedUser = userRepository.save(user);
 		
-		return UserMapper.usertoUserDto(savedUser); 
+//		return UserMapper.usertoUserDto(savedUser); 
+		return MapStructMapper.Mapper.mapToUserDto(savedUser);
 	}
 
 	@Override
 	public UserDto getUserById(Long id) {
 		
 		Optional<User> user = userRepository.findById(id);
-		UserDto userreturn = UserMapper.usertoUserDto(user.get());
+		//ussing custom mapper
+//		UserDto userreturn = UserMapper.usertoUserDto(user.get());
+		
+		// using modelmapper
+//		UserDto userreturn = modelMapper.map(user.get(), UserDto.class);
+		
+		//using mapstruct
+		UserDto userreturn = MapStructMapper.Mapper.mapToUserDto(user.get());
+		
 		return userreturn;
 	}
 
 	@Override
 	public List<UserDto> getAllUsers() {
 		List<User> list = userRepository.findAll();
-		return list.stream().map(UserMapper::usertoUserDto)
-					.collect(Collectors.toList());
+		
+//		ussing custom mapper
+//		return list.stream().map(UserMapper::usertoUserDto)
+//					.collect(Collectors.toList());
+		
+		//Using modelmapper
+//		return list.stream().map((u)-> modelMapper.map(u, UserDto.class))
+//				.collect(Collectors.toList());
+		
+		//using mapstruct
+		return list.stream().map(MapStructMapper.Mapper::mapToUserDto)
+				.collect(Collectors.toList());
 	}
 
 	@Override
@@ -57,8 +87,14 @@ public class UserServiceImpl implements UserService{
 		existingUser.setEmail(user.getEmail());
 		
 		User updatedUser = userRepository.save(existingUser);
+//		ussing custom mapper
+//		return UserMapper.usertoUserDto(updatedUser);
 		
-		return UserMapper.usertoUserDto(updatedUser);
+//		//using modelmapper
+//		return modelMapper.map(updatedUser,UserDto.class);
+		
+		//using mapstruct
+		return MapStructMapper.Mapper.mapToUserDto(updatedUser);
 	}
 
 	@Override
