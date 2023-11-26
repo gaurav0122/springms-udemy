5ae787096501cc8f18480e71e35e97acce904c2f
Gaurav <gauravlavand0102@gmail.com>
2023-11-26 16:22:30 +0530

    addding till api gateway

---
+++ Config-Servver/src/main/java/com/config/servver/ConfigServverApplication.java
@@ -0,0 +1,13 @@
+package com.config.servver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServverApplication.class, args);
+	}
+
+}
