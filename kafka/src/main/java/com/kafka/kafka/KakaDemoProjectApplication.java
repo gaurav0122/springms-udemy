f17b1dfa0114a1945bf5f727a070119c75a539e9
Gaurav <gauravlavand0102@gmail.com>
2023-11-27 17:57:03 +0530

    adding kafka till section 30

---
+++ kafka/src/main/java/com/kafka/kafka/KakaDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaDemoProjectApplication.class, args);
+	}
+
+}
---
+++ kafka/src/test/java/com/kafka/kafka/KakaDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
