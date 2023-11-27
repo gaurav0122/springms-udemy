ca58c1f044e8f2755ba0f4f2b3f3980e37074b17
Gaurav <gauravlavand0102@gmail.com>
2023-11-27 13:02:01 +0530

    task during the call

--- A/src/co/in/Abc.java
+++ A/src/co/in/Abc.java
@@ -1,7 +1,18 @@
 package co.in;
 
+import java.util.Scanner;
+
 public class Abc {
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a numbner");
+		int n = sc.nextInt();
+		System.out.println(n);
+		int add =0;
+		while(n>0) {
+			add+=(n%10);
+			n/=10;
+		}
+		System.out.println(add);
 	}
 }
