17fe11335c8c2c91e59b41f67bf24c279b7a5888
Gaurav <gauravlavand0102@gmail.com>
2023-12-01 12:01:41 +0530

    rabbit mq added

---
+++ Case-study/email-rabbit-mq/src/main/java/com/rabbit/EmailRabbitMqApplication.java
@@ -0,0 +1,13 @@
+package com.rabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailRabbitMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailRabbitMqApplication.class, args);
+	}
+
+}
